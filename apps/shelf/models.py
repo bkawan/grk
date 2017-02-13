bdeed38b641c096575210040f2be637c6824bab9
Roshan Shrestha <roshanshrestha01@gmail.com>
2017-02-13 16:39:06 +0545

    shelf in INSTALLED_APPS and initial models

--- apps/shelf/models.py
+++ apps/shelf/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Book(models.Model):
+    pass
+
+
--- grk/settings.py
+++ grk/settings.py
@@ -9,6 +9,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'apps.shelf',
 ]
 
 MIDDLEWARE = [
