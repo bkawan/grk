da1e35e84c281394c065c6bb64bb09b96b2f653e
Roshan Shrestha <roshanshrestha01@gmail.com>
2017-02-13 16:49:09 +0545

    Author, Category, Tag and Book model with few initial attributes

--- apps/shelf/models.py
+++ apps/shelf/models.py
@@ -2,14 +2,22 @@ from django.db import models
 
 
 class Author(models.Model):
-    pass
+    name = models.CharField(max_length=250)
+    avatar = models.ImageField(upload_to='avatar/', blank=True, null=True)
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=250)
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=250)
 
 
 class Book(models.Model):
-    pass
-
+    title = models.CharField(max_length=255)
+    thumbnail = models.ImageField(upload_to='thumbnail', blank=True, null=True)
+    category = models.ForeignKey(Category, blank=True, null=True)
+    tags = models.ManyToManyField(Tag, blank=True, null=True)
+    author = models.ForeignKey(Author, related_name='books')
 
