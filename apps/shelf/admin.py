2c78b983323e2d672916f80e47414e909515335e
Roshan Shrestha <roshanshrestha01@gmail.com>
2017-02-14 14:41:40 +0545

    Publisher, Identifiers and TableOfContent added

--- apps/shelf/admin.py
+++ apps/shelf/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
-from .models import Category, Author, Book, Tag
+from .models import Category, Author, Book, Tag, Publisher, Identifiers, TableOfContent
 
 admin.site.register(Category)
 admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(Tag)
+admin.site.register(Publisher)
+admin.site.register(Identifiers)
+admin.site.register(TableOfContent)
 # Register your models here.
--- apps/shelf/models.py
+++ apps/shelf/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-
 class Genre(models.Model):
     name = models.CharField(max_length=250)
 
 
 class Author(models.Model):
     name = models.CharField(max_length=250)
-    genre = models.ManyToManyField(Genre, blank=True, null=True)
+    genre = models.ManyToManyField(Genre, blank=True)
     website = models.URLField(blank=True, null=True)
     info = models.TextField(blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
@@ -21,16 +20,42 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
+
 class Tag(models.Model):
     title = models.CharField(max_length=250)
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=250)
 
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
     thumbnail = models.ImageField(upload_to='thumbnail', blank=True, null=True)
     description = models.TextField()
-    isbn = models.PositiveIntegerField(unique=True)
+    isbn_10 = models.PositiveIntegerField(unique=True)
+    isbn_13 = models.PositiveIntegerField(unique=True)
+    number_of_pages = models.IntegerField()
+    weight = models.IntegerField()
+    language = models.CharField(max_length=250, blank=True, null=True)
+    oclc_number = models.IntegerField()
+    physical_dimensions = models.CharField(max_length=100,blank=True, null=True)
+    revision = models.IntegerField()
+    publishers = models.ForeignKey(Publisher, blank=True, null=True)
+    published_date = models.DateField()
+    pagination = models.IntegerField(blank=True, null=True)
+    lccn = models.IntegerField()
     category = models.ForeignKey(Category, blank=True, null=True)
-    tags = models.ManyToManyField(Tag, blank=True, null=True)
+    tags = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(Author, related_name='books')
 
+
+class Identifiers(models.Model):
+    name = models.CharField(max_length=250)
+    code = models.CharField(max_length=250)
+    book = models.ForeignKey(Book, related_name='identifiers')
+
+
+class TableOfContent(models.Model):
+    title = models.CharField(max_length=250)
+    book = models.ForeignKey(Book, related_name='table_of_contents')
