c20177567507a81c6342a1b16b0358cd812ead51
Roshan Shrestha <roshanshrestha01@gmail.com>
2017-02-14 15:02:11 +0545

    admin register Language

--- apps/shelf/admin.py
+++ apps/shelf/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Author, Book, Tag, Publisher, Identifiers, TableOfContent
+from .models import Category, Author, Book, Tag, Publisher, Identifiers, TableOfContent, Language
 
 admin.site.register(Category)
 admin.site.register(Author)
@@ -9,4 +9,5 @@ admin.site.register(Tag)
 admin.site.register(Publisher)
 admin.site.register(Identifiers)
 admin.site.register(TableOfContent)
+admin.site.register(Language)
 # Register your models here.
