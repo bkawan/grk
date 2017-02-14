3304a109310dbc76100611b4738d36606ff9afae
Roshan Shrestha <roshanshrestha01@gmail.com>
2017-02-14 13:37:42 +0545

    admin regiter shelf app

--- apps/shelf/admin.py
+++ apps/shelf/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Category, Author, Book, Tag
+
+admin.site.register(Category)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Tag)
 # Register your models here.
